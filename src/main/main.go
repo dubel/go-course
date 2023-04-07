723eb1e18eede2867de1ccd99af4102a974bdf03
Michał Dubel <Michal.Dubel@ttpsc.pl>
2023-04-07 12:51:22 +0200

    Playing around

--- src/main/main.go
+++ src/main/main.go
@@ -3,5 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello, world!")
+	ptr := new(int)
+	*ptr = 4
+	text := "ala"
+	text = "blo"
+	integer := 5
+	type Workday int
+	const (
+		Monday Workday = iota
+		Tuesday
+		Wednesday
+	)
+
+	switch integer {
+	case 1:
+		fmt.Printf("1!")
+	case 2:
+		fmt.Printf("1!")
+	}
+	fmt.Printf("Hello, world! %s", text)
 }
