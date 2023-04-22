962a305ef088089792918dd76e0f184f260158c9
dubel <dubel.michal@gmail.com>
2023-04-22 21:07:16 +0200

    Final assingment of first course

--- src/main/structs.go
+++ src/main/structs.go
@@ -1,13 +1,13 @@
 package main
 
-type Person struct {
+type PersonOld struct {
 	name  string
 	addr  string
 	phone string
 }
 
 func main() {
-	p1 := new(Person)
+	p1 := new(PersonOld)
 	p1.name = "小明"
 	p1.addr = "北今"
 	p1.phone = "13888888888"
