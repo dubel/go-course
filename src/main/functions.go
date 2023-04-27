6fbc3a3783c9df7e2eb3d1b898db3bc0674c0dd6
dubel <dubel.michal@gmail.com>
2023-04-27 16:05:59 +0200

    Passing arrays to functions

--- src/main/functions.go
+++ src/main/functions.go
@@ -2,8 +2,23 @@ package main
 
 func main() {
 	println(foo(1, 2))
+	a := [3]int{1, 2, 3}
+	println(bar(a))
+	println(a[0])
+	println(baz(&a))
+	println(a[0])
 }
 
 func foo(x, y int) (int, int) {
 	return x + y, x - y
+}
+
+func bar(x [3]int) int {
+	x[0] = 666
+	return x[0]
+}
+
+func baz(x *[3]int) int {
+	(*x)[0] = 666
+	return (*x)[0]
 }
