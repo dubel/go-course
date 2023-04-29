f960608bb7aa49e996db62020f53014553e351f7
dubel <dubel.michal@gmail.com>
2023-04-30 00:10:24 +0200

    Bubble Sort implementation finished

--- src/main/bubbleSort.go
+++ src/main/bubbleSort.go
@@ -1,40 +1,52 @@
 package main
 
-/*
-Write a Bubble Sort program in Go. The program
-should prompt the user to type in a sequence of up to 10 integers. The program
-should print the integers out on one line, in sorted order, from least to
-greatest. Use your favorite search tool to find a description of how the bubble
-sort algorithm works.
-
-As part of this program, you should write a
-function called BubbleSort() which
-takes a slice of integers as an argument and returns nothing. The BubbleSort() function should modify the slice so that the elements are in sorted
-order.
-
-A recurring operation in the bubble sort algorithm is
-the Swap operation which swaps the position of two adjacent elements in the
-slice. You should write a Swap() function which performs this operation. Your Swap()
-function should take two arguments, a slice of integers and an index value i which
-indicates a position in the slice. The Swap() function should return nothing, but it should swap
-the contents of the slice in position i with the contents in position i+1.
-*/
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
 func BubbleSort(s []int) {
 	for i := 0; i < len(s); i++ {
 		for j := 0; j < len(s)-1; j++ {
 			if s[j] > s[j+1] {
-				s[j], s[j+1] = s[j+1], s[j]
-				//swap(s, j, j+1)
+				Swap(s, j)
 			}
 		}
 	}
 }
 
-func Swap(s []int, i, j int) {
-	s[i], s[j] = s[j], s[i]
+func Swap(s []int, i int) {
+	if i >= len(s)-1 {
+		return
+	}
+	temp := s[i]
+	s[i] = s[i+1]
+	s[i+1] = temp
 }
 
 func main() {
-
+	fmt.Println("Please enter a sequence of up to 10 integers that would be sorted by Bubble Sort.")
+	inputNumbersSlice := make([]int, 0, 10)
+	var inputNumberAsString string
+	for i := 0; i < 10; i++ {
+		fmt.Println("Please input integer value or 'X' to end the program execution:")
+		tokensNumber, err := fmt.Scan(&inputNumberAsString)
+		if strings.ToUpper(inputNumberAsString) == "X" {
+			break
+		}
+		inputNumberAsInt, err := strconv.Atoi(inputNumberAsString)
+		if err != nil {
+			fmt.Println("Error during conversion")
+			continue
+		}
+		if err == nil && tokensNumber == 1 {
+			inputNumbersSlice = append(inputNumbersSlice, inputNumberAsInt)
+			BubbleSort(inputNumbersSlice)
+			fmt.Println("Slice sorted after adding new value:", inputNumbersSlice)
+		} else {
+			break
+		}
+	}
+	fmt.Println("Program execution finished. Final content of sorted slice:", inputNumbersSlice)
 }
