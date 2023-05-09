12b41901a5f7e7da514ddf32bf60c47ec2bad94c
dubel <dubel.michal@gmail.com>
2023-05-09 21:04:42 +0200

    Playing around with prime generator

--- src/main/prime_goroutine.go
+++ src/main/prime_goroutine.go
@@ -1,34 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-// Generate the input values passing through the go channel pipe
-func generate(ch chan int) {
-	go func() {
-		for i := 2; ; i++ {
-			ch <- i
-		}
-	}()
+func generate(ch chan<- int) {
+	for i := 2; ; i++ {
+		ch <- i
+	}
 }
 
-// Copy the value from input channel to output channel
-func filter(in chan int, out chan int, filter int) {
-	for val := range in {
-		if val%filter != 0 {
-			out <- val // send value to the output pipe
+func filter(src <-chan int, dst chan<- int, prime int) {
+	for i := range src {
+		if i%prime != 0 {
+			dst <- i
 		}
 	}
+}
+
+func sieve(ch chan int) {
+	prime := <-ch
+	fmt.Println(prime)
+
+	ch1 := make(chan int)
+	go filter(ch, ch1, prime)
+	sieve(ch1)
 }
 
 func main() {
-	ch := make(chan int) // create a new a channel
-	generate(ch)
+	var numThreads int
+	fmt.Print("Enter the number of threads: ")
+	fmt.Scan(&numThreads)
 
-	for {
-		prime := <-ch
-		fmt.Println(prime, " ")
-		ch1 := make(chan int)
-		go filter(ch, ch1, prime)
-		ch = ch1
+	ch := make(chan int)
+	go generate(ch)
+	for i := 0; i < numThreads; i++ {
+		go sieve(ch)
 	}
+
+	// Wait for user to press enter
+	fmt.Println("Press Enter to exit...")
+	fmt.Scanln()
 }
