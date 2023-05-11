f0df62fafd30591cd76ef578571285b6c470016b
dubel <dubel.michal@gmail.com>
2023-05-11 13:28:52 +0200

    Interfaces examples

---
+++ src/main/interfaces.go
@@ -0,0 +1,24 @@
+package main
+
+type Shape2D interface {
+	Area() float64
+	Perimeter() float64
+}
+
+type Triangle struct {
+	base   float64
+	height float64
+	color  string
+}
+
+func (t Triangle) Area() float64 {
+	return t.base * t.height / 2
+}
+
+func (t Triangle) Perimeter() float64 {
+	return 0
+}
+
+func main() {
+
+}
