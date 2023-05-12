f9a6bf4ecedbf2ae11d15ab3a9a6075b8c2ad713
dubel <dubel.michal@gmail.com>
2023-05-12 14:41:58 +0200

    Final animal assignment solution

--- src/main/animals_creation.go
+++ src/main/animals_creation.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 type Animal interface {
 	Speak()
@@ -9,6 +12,59 @@ type Animal interface {
 }
 
 func main() {
+
+	fmt.Println("Please enter the newanimal or query command \">\" in one (ex. newanimal <name> <type> or query <name> <activity>: ")
+	fmt.Println("ex. newanimal tom bird, query tom speak. syntax is: newanimal <name> [bird|snake|cow] and query <name> [eat|speak|move]")
+	fmt.Println("Use sigkill to end program execution - ctrl/cmd + C")
+	bird := Bird{"worms", "fly", "peep"}
+	cow := Cow{"grass", "walk", "moo"}
+	snake := Snake{"mice", "slither", "hsss"}
+
+	var cmd, name, parameter string
+	var animalsMap = make(map[string]Animal)
+	for {
+		fmt.Print(">")
+		if _, err := fmt.Scanln(&cmd, &name, &parameter); err != nil {
+			fmt.Println("Error:", err)
+			break
+		}
+		switch cmd {
+		case "newanimal":
+			if animalsMap[name] == nil {
+				var newAnimal Animal
+				switch parameter {
+				case "bird":
+					newAnimal = bird
+				case "cow":
+					newAnimal = cow
+				case "snake":
+					newAnimal = snake
+				default:
+					fmt.Println("unrecognized animal")
+					continue
+				}
+				animalsMap[name] = newAnimal
+				fmt.Println("Created it！")
+			}
+		case "query":
+			animal := animalsMap[name]
+			if animal == nil {
+				fmt.Println("Animal not found!")
+				continue
+			}
+			switch strings.ToUpper(parameter) {
+			case "EAT":
+				animal.Eat()
+			case "MOVE":
+				animal.Move()
+			case "SPEAK":
+				animal.Speak()
+			}
+		default:
+			fmt.Println("Unrecognized command")
+			continue
+		}
+	}
 
 }
 
