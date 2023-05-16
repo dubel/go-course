f4cb467008b24286385755eebc0244c23462cefd
dubel <dubel.michal@gmail.com>
2023-05-16 12:35:10 +0200

    Added default case for selecting channels

--- src/main/select_channels.go
+++ src/main/select_channels.go
@@ -10,10 +10,13 @@ func main() {
 	c := make(chan int)
 	go product(1, 2, c)
 	go product(3, 4, c)
+	// whatever the first value is received, it is printed
 	select {
 	case a := <-c:
 		fmt.Println(a)
 	case b := <-c:
 		fmt.Println(b)
+	default:
+		fmt.Println("nothing")
 	}
 }
